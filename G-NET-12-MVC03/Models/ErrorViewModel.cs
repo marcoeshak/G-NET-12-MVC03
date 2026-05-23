ff021e214afcb1b219c821106451311d873aafd2
marco <marco@DESKTOP-GCUIOA1>
2026-05-23 22:30:28 +0300

    Add project files.

---
+++ G-NET-12-MVC03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace G_NET_12_MVC03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ G-NET-12-MVC03/Models/Plan.cs
@@ -0,0 +1,10 @@
+﻿    namespace G_NET_12_MVC03.Models
+{
+    public class Plan
+    {
+        public int Id { get; set; }
+
+
+
+    }
+}
